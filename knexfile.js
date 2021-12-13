3861230ecbdaf41087afc49af7f2cd1b4cc63108
ldjmoreira <ldjmoreira@hotmail.com>
2021-12-12 21:56:06 -0300

    heroku try 1 pg

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@
 module.exports = {
 	
 	client: 'postgresql',
-	connection: process.env.DATABASE_URL,
+	connection: 'postgres://snjowmsoejvgha:cdcb934e471abb4a45e69b350690125eb5be1b3e4ca9e952098a1be73865d60f@ec2-3-217-216-13.compute-1.amazonaws.com:5432/dd1tdrib1nauud',
 	/*
 	connection: {
 		host : '127.0.0.1',
